df7e54784b59c3ca02d7e7c84b8800432034d1f2
Michael Chew <michael.chew@actifio.com>
2019-12-09 15:17:51 +1100

    Add files via upload

---
+++ act-pipeline-runcommands/rpt_health.ps1
@@ -0,0 +1,39 @@
+##
+## File: rpt_health.ps1
+##
+## Prerequisites:
+##   - Parameters defined in Jenkins build job:
+##     : ActUser - Actifio user
+##     : ActPass - password for the above Actifio user
+##     : ActIP - IP address or FQDN of the Actifio appliance
+##
+
+param(
+[string]$ActUser,
+[string]$ActPass,
+[string]$ActIP
+)
+
+$LocalTempDir = "c:\temp\"
+If(!(test-path $LocalTempDir)) {
+    New-Item -ItemType Directory -Force -Path $LocalTempDir | out-null
+    }
+    
+$TmpPasswdFile = "$LocalTempDir\$env:USERNAME-passwd.key"
+"$ActPass" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File $TmpPasswdFile
+
+if (! $env:ACTSESSIONID ){
+   Connect-Act $ActIP -actuser $ActUser -passwordfile $TmpPasswdFile -ignorecerts
+}
+
+
+if (! $env:ACTSESSIONID ){
+   write-warning "Login to VDP $ActIP failed"
+   break
+ }
+ else {
+    reporthealth | select-object -property status, checkname
+    Disconnect-Act | Out-Null
+ } 
+
+rm "$TmpPasswdFile" -ErrorAction SilentlyContinue 
